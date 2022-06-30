35f164f775b052abc24588dac7375f53461aa3f7
Shahaf <96386373+Shahsf@users.noreply.github.com>
2022-06-30 23:31:46 +0300

    Add files via upload

---
+++ Mp3_Music_Download.py
@@ -0,0 +1,23 @@
+from mp3download import mp3download
+import ssss
+
+with open('C://Mp3_Music_Download//music.txt') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        output = []
+        for line in f:
+            line = line.rstrip()
+            output.append(line)
+count = 0
+if not output:
+    print("No Songs were picked (Enter Songs name in Text File)")
+    quit()
+
+else:
+
+    for word in output:
+        songname = word
+        get_url = ssss.downloadssong(songname)
+        mp3download(get_url)
